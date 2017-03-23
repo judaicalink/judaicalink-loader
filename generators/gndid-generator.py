e1add334e0e47767a4e61bd98fa67429f7f00ae7
Maral Dadvar <dadvar.maral@gmail.com>
2017-03-23 11:22:19 +0100

    script for generating gndid of generated persons

--- generators/gndid-generator.py
+++ generators/gndid-generator.py
@@ -1,6 +1,6 @@
 #Maral Dadvar
-#15/03/2017
-#This scrips reads the rdf files generated for person from dbpedia and enriches each file with GNDid URI from Wikidata.
+#23/03/2017
+#This script reads the rdf file generated for person from dbpedia and generates the GNDid URI from Wikidata for the persons.
 
 import unicodedata
 import os , glob
@@ -14,14 +14,8 @@ os.chdir('C:\Users\Maral\Desktop\output')
 
 sparql = SPARQLWrapper2("https://query.wikidata.org/sparql")
 
-path = 'C:\Users\Maral\Desktop\generated_person' #adapted to the list file path
-
-output = open('statistics.csv','w')
-
-output.writelines([str('filename'),',',str('gnd'),',',str('nognd'),'\n'])
-
-
 graph = Graph()
+
 
 foaf = Namespace("http://xmlns.com/foaf/0.1/")
 rdf = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
@@ -30,9 +24,9 @@ gndo = Namespace("http://d-nb.info/standards/elementset/gnd#")
 
 wikilist=[]
 
-def GndGenerator (URI):
+def generator_gndid (URI):
 
-    "this function extracts the gndid of the author from Wikidata"
+    "this function extracts the gndid of the person from Wikidata"
 
     spar1= """
         PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
@@ -73,68 +67,59 @@ def GndGenerator (URI):
            return b[u"id"].value
 
 
-for filename in glob.glob(os.path.join(path, '*.rdf')):
 
-    ngnd = 0
-    nognd=0
-    print filename
-    g = Graph()
-    g.parse(filename, format="turtle")
+g = Graph()
+g.parse('C:\Users\Maral\Desktop\generated_person\generated_person.rdf', format="turtle")
 
-    #this query will extract only the sameAs links to Wikidata
+#this query will extract only the sameAs links to Wikidata
 
-    spar= """
-        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        PREFIX gndo: <http://d-nb.info/standards/elementset/gnd#>
-        PREFIX pro: <http://purl.org/hpi/patchr#>
-        PREFIX owl: <http://www.w3.org/2002/07/owl#>
-        PREFIX edm: <http://www.europeana.eu/schemas/edm/>
-        PREFIX dc: <http://purl.org/dc/elements/1.1/>
-        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-        PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX dblp: <http://dblp.org/rdf/schema-2015-01-26#>
-        PREFIX dcterms: <http://purl.org/dc/terms/>
-        PREFIX dbpedia: <http://dbpedia.org/resource/>
+spar= """
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX gndo: <http://d-nb.info/standards/elementset/gnd#>
+    PREFIX pro: <http://purl.org/hpi/patchr#>
+    PREFIX owl: <http://www.w3.org/2002/07/owl#>
+    PREFIX edm: <http://www.europeana.eu/schemas/edm/>
+    PREFIX dc: <http://purl.org/dc/elements/1.1/>
+    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX dblp: <http://dblp.org/rdf/schema-2015-01-26#>
+    PREFIX dcterms: <http://purl.org/dc/terms/>
+    PREFIX dbpedia: <http://dbpedia.org/resource/>
 
-        SELECT ?x ?same
+    SELECT ?x ?same
 
-        where {
+    where {
 
-          ?x owl:sameAs ?same.
-          FILTER ( strstarts(str(?same), "http://www.wikidata.org/entity/")). }
+      ?x owl:sameAs ?same.
+      FILTER ( strstarts(str(?same), "http://www.wikidata.org/entity/")). }
 
-    """
+"""
 
-    results = g.query(spar)
+results = g.query(spar)
 
-    g.bind('gndo',gndo)
+graph.bind('gndo',gndo)
+graph.bind('foaf',foaf)
 
-    for item in results:
-
-       URI = '<'+ str(item[1]) + '>' #turn it into query adaptable URI format
-
-       wikilist.append(URI)
-
-       gnd = GndGenerator(URI) #generate the GNDid
-
-       outputfile = 'gnd_' + filename.rsplit('\\',1)[1].strip() #creat the output file name
-
-       if gnd != None: #if an id was found
-            ngnd = ngnd+1 #to count the number of entries with an id
-            gndid = 'http://d-nb.info/gnd/' + gnd
-            print gndid
-
-            g.add( (URIRef(item[0]), gndo.gndIdentifier , URIRef(gndid) ) ) #add the GNDid to the graph
-            g.serialize(destination = outputfile , format="turtle")
-
-       else:
-            nognd = nognd +1 #to count the number of entries without an id
-            g.serialize(destination = outputfile , format="turtle")
-
-    output.writelines([str(filename),',',str(ngnd),',',str(nognd),'\n'])
+for item in results:
 
 
-output.close()
+   URI = '<'+ str(item[1]) + '>' #turn it into query adaptable URI format
 
+   wikilist.append(URI)
+
+   gnd = generator_gndid(URI) #generate the GNDid
+
+
+   if gnd != None: #if an id was found
+
+        gndid = 'http://d-nb.info/gnd/' + gnd
+        print gndid
+
+        graph.add( (URIRef(item[0]), RDF.type , foaf.Person ) )
+        graph.add( (URIRef(item[0]), gndo.gndIdentifier , URIRef(gndid) ) ) #add the GNDid to the graph
+
+
+
+graph.serialize(destination = 'generated_gndid.rdf' , format="turtle")
 
