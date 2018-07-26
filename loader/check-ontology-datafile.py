196f1a8069b8a0820f1c9af463d4cfaf982578e9
Maral Dadvar <dadvar.maral@gmail.com>
2018-07-26 13:52:07 +0200

    checking the data file ontology script

---
+++ loader/check-ontology-datafile.py
@@ -0,0 +1,58 @@
+#This script reads the RDF ontology and an RDF dataset file (both in turtle) and outputs statistics about all properties used in this file,
+#distinguishing properties that are in our ontology and the ones that are not.
+
+from rdflib import Graph
+from rdflib.namespace import RDF
+import sys
+from SPARQLWrapper import SPARQLWrapper2, XML , RDF , JSON , TURTLE
+
+
+def ontolgoy_checker(ontology,datafile):
+
+    ont = Graph()
+    ont.parse(ontology, format="turtle")
+    data = Graph()
+    data.parse(datafile, format="turtle")
+    ont.bind('rdf',RDF)
+
+    result = ont.query("SELECT ?p WHERE {?p a rdf:Property}")
+
+    properties = [str(b["p"]) for b in result.bindings]
+
+    print("Checking all properties in our ontology:")
+    for p in properties:
+        print(p)
+
+    result = data.query("SELECT ?p (count(?p) as ?c) WHERE {?s ?p ?o} GROUP BY ?p")
+    dataprops = [(str(b["p"]), str(b["c"])) for b in result.bindings]
+    print("Checking all properties in our dataset:")
+    in_ontology = {}
+    undocumented = {}
+    for p in dataprops:
+        if p[0] in properties:
+            in_ontology[p[0]] = p[1]
+        else:
+            undocumented[p[0]] = p[1]
+
+    print("Ontology statistics:")
+    for i in range (0,len(in_ontology.items())):
+        print(in_ontology.items()[i])
+
+    print("Not in ontology:")
+    print(undocumented)
+
+    return
+
+
+#insert the file name from the commant line
+ontology = sys.argv[1]
+datafile = sys.argv[2]
+
+#insert the file name directly
+#ontology = 'C:\Users\Maral\Desktop\judaicalink-ontology.ttl'
+#datafile = 'C:\\Users\\Maral\\Desktop\\bhr-final-05.ttl'
+
+ontolgoy_checker(ontology,datafile)
+
+
+
--- loader/ontology-md-to-rdf.py
+++ loader/ontology-md-to-rdf.py
@@ -1,28 +1,37 @@
-##This script takes the judaicalink-ontology.md file as first argument/input file 
-##and outputs only the code parts as an rdf file.
+#This script takes the judaicalink-ontology.md file as first argument/input file and outputs only the code parts as an rdf file.
 
 import sys
 import os
+import re
 
-#f = open('C:\Users\Maral\Desktop\judaicalink-ontology.md', 'r') #insert the file name directly
+def ontology_rdf(ontologyfile ):
 
-files = sys.argv[1] #insert the file name from the commant line
-f = open(files, 'r')
+    f = open(ontologyfile, 'r')
+    output = open('judaicalink-ontology.ttl','w')
 
-output = open('judaicalink-ontology.ttl','w')
+    incode = False
+    for line in f:
 
-incode = False
-for line in f:
-
-    if re.match(r'^\s*$', line):
-        continue
-    elif line.startswith("```"):
-        incode = not incode
-        continue
-    elif incode:
-        print(line)
-        output.write(line)
+        if re.match(r'^\s*$', line):
+            continue
+        elif line.startswith("```"):
+            incode = not incode
+            continue
+        elif incode:
+            print(line)
+            output.write(line)
 
 
-output.close()
+    output.close()
+    return
+
+
+#insert the file name directly
+ontologyfile ='C:\Users\Maral\Desktop\judaicalink-ontology.md'
+
+#insert the file name from the commant line
+#ontologyfile = sys.argv[1]
+
+ontology_rdf(ontologyfile)
+
 
